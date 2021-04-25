f757d3e98daaeb57906a6e8678ed909fb2c3bec9
Sk Azaharuddin <skazaharuddin@mvista.com>
2021-04-25 17:25:00 +0530

    Subject: [PATCH] client-io: adding helloworld feature
        Source: HCL
        MR: 0001
        Type: Integration
        Disposition: Local
        ChangeID: 79a02ac254eece125b83b35d6412f0883caa5571
        Description:
    
        Adding helloworld feature
    
        The allocated memory is then map to cleine-IO package  and can be
        verified by running program output-terminal
    
        Signed-off-by: Indrani Jana <in.jana09@gmail.com>

---
+++ helloworld-1.java
@@ -0,0 +1,19 @@
+
+/*****************************************************************************
+ * Author: Indrani Jana <in.jana09@gmail.com>
+ * License: MIT
+ * (C) Copyright HCL 2019-2020. All rights reserved. 
+ * File Description: print Helloworld on display
+ *        
+*****************************************************************************/
+ 
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
+
